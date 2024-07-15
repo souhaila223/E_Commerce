bf4217dbdca46c8d099c13372502fdb6dad7bf72
souhaila223 <souhaila.elmerabet@final.edu.tr>
2024-07-15 14:37:29 +0300

    Integration with API for all products in the HomePage + Register page

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,5 +1,43 @@
+import { Box, Grid } from "@mui/material";
+import Container from "@mui/material/Container";
+import ProductCard from "../components/ProductCard";
+import { useEffect, useState } from "react";
+import { Product } from "../types/Product";
+import { Base_URL } from "../constants/baseUrl";
+
 const HomePage = () => {
-    return <div>Home Page</div>;
+    const [products, setProducts] = useState<Product[]>([])
+    const [error, setError] = useState(false);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch(`${Base_URL}/product`);
+                const data = await response.json();
+                setProducts(data);
+            } catch {
+                setError(true);
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    if(error) {
+        return <Box>Something went wrong, please try again!</Box>
+    }
+    return (
+         <Container sx={{ mt:2}}>
+            <Grid container spacing={2}>
+                {products.map((p) => (
+                    <Grid item md={4}>
+                    <ProductCard {...p}/>
+                  </Grid>
+                ))}
+
+            </Grid>
+        </Container>
+    );
 };
 
 export default HomePage;  
