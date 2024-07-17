fc7539c11f0ebf505421d32b721af0e3514e2ed0
souhaila223 <souhaila.elmerabet@final.edu.tr>
2024-07-17 23:44:16 +0300

    Cart Context + Cart Provider

--- frontend/src/components/ProductCard.tsx
+++ frontend/src/components/ProductCard.tsx
@@ -5,6 +5,7 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import { useCart } from "../context/Cart/CartContext";
 
 interface Props {
     _id: string;
@@ -13,7 +14,8 @@ interface Props {
     price: string;
 }
 
-export default function ProductCard({ title, image, price}: Props) {
+export default function ProductCard({ _id,title, image, price}: Props) {
+  const { addItemToCart} = useCart();
   return (
     <Card>
       <CardMedia
@@ -30,7 +32,7 @@ export default function ProductCard({ title, image, price}: Props) {
         </Typography>
       </CardContent>
       <CardActions>
-        <Button variant="contained" size="small">Add to Cart</Button>
+        <Button variant="contained" size="small" onClick={() => addItemToCart(_id)}>Add to Cart</Button>
       </CardActions>
     </Card>
   );
