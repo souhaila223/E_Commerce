c6d6505305490d87eabdf5829196d6878682b726
souhaila223 <souhaila.elmerabet@final.edu.tr>
2024-10-29 14:11:06 +0200

    Database URL update

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(cors());
 
 mongoose
-  .connect("mongodb://localhost:27017/e-commerce")
+  .connect(process.env.DATABASE_URL ||"mongodb://localhost:27017/e-commerce")
   .then(() => console.log("Mongo connected!"))
   .catch((err) => console.log("Failed to connect ", err));
 
